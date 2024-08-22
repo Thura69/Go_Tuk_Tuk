58843d2526d1706b053c17c8b7c4f6ac1bcd3937
Thura69 <spyking64@gmail.com>
2024-08-22 20:35:23 +0630

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,4 @@
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "./components/ui/card"
-
 import './App.css'
 import { CardWithForm } from "./components/ui/login_card"
 
