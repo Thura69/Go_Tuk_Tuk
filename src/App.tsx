21021a94e269c93168ecb60770e86b43e3319959
Thura69 <spyking64@gmail.com>
2024-08-22 19:41:49 +0630

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./components/ui/card"
+
+import './App.css'
+import { CardWithForm } from "./components/ui/login_card"
+
+function App() {
+  
+  return (
+    <>
+   <CardWithForm/>
+
+    </>
+  )
+}
+
+export default App
